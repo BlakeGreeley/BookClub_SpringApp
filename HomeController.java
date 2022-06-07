4bd71af3c62213fa648556da5d850ad6736a246d
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-06-07 14:17:39 -0700

    Update HomeController.java

--- HomeController.java
+++ HomeController.java
@@ -21,99 +21,143 @@ import com.codingdojo.bookclub.services.UserService;
 
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private UserService users;
-	
-	@Autowired
-	private BookService books;
-	
-	@GetMapping("/")
-	public String index(Model model) {
-		model.addAttribute("newUser", new User());
-		model.addAttribute("newLogin", new LoginUser());
-		return "index.jsp";
-	}
-	
-	@PostMapping("/register")
-	public String register(@Valid @ModelAttribute("newUser") User newUser, BindingResult result, Model model, HttpSession session) {
-		User user = users.register(newUser, result);
-		if(result.hasErrors()) {
-			model.addAttribute("newLogin", new LoginUser());
-			return "index.jsp";
-		}
-	}
-	
-	@PostMapping("/login")
-	public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, BindingResult result, Model model, HttpSession session) {
-		User user = users.login(newLogin, result);
-		if(result.hasErrors()) {
-			model.addAttribute("newUser", new User());
-			return "index.jsp";
-		}
-		session.setAttribute("userId", user.getId());
-		return "redirect:/home";
-	}
-	
-	@GetMapping("/home")
-	public String home(Model model, HttpSession session) {
-		if(session.getAttribute("user.id") == null) {
-			return "redirect:/";
-		}
-		model.addAttribute("books", books.all());
-		model.addAttribute("user", users.findById(Long)session.getAttribute("userId")));
-		return "home.jsp";
-	}
-	
-	@GetMapping("/new")
-	public String addPage(@ModelAttribute("book") Book book, BindingResult result) {
-		if (result.hasErrors()) {
-			return "new.jsp";
-		}
-		books.create(book);
-		return "redirect:/home";
-	}
-	
-	@GetMapping("books/{id}")
-	public String showPage(Model model, @PathVariable("id") Long id, HttpSession session) {
-		if(session.getAttribute("userId") == null) {
-			return "redirect:/";
-		}
-		Book book = books.findById(id);
-		model.addAttribute("book", book);
-		model.addAttribute("user", users.findById(Long)session.getAttribute("userId")));
-		return "show.jsp";
-	}
-	
-	@PutMapping("/books/{id}")
-    public String editBook(@Valid @ModelAttribute("book") Book book, BindingResult result, Model model) {
+
+	// Add once service is implemented:
+     @Autowired
+     private UserService users;
+     @Autowired
+     private BookService books;
+    
+    @GetMapping("/")
+    public String index(Model model) {
+    
+        // Bind empty User and LoginUser objects to the JSP
+        // to capture the form input
+        model.addAttribute("newUser", new User());
+        model.addAttribute("newLogin", new LoginUser());
+        return "index.jsp";
+    }
+    
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("newUser") User newUser, 
+            BindingResult result, Model model, HttpSession session) {
+        
+        // Call a register method in the service 
+        // to do some extra validations and create a new user!
+    	User user = users.register(newUser, result);
+    	
+        
+        if(result.hasErrors()) {
+            // Be sure to send in the empty LoginUser before 
+            // re-rendering the page.
+            model.addAttribute("newLogin", new LoginUser());
+            return "index.jsp";
+        }
+        
+        // No errors! 
+        // TO-DO Later: Store their ID from the DB in session, 
+        // in other words, log them in.
+        session.setAttribute("userId", user.getId());
+    
+        return "redirect:/home";
+    }
+    
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
+            BindingResult result, Model model, HttpSession session) {
+        
+        // Add once service is implemented:
+        User user = users.login(newLogin, result);
+    
+        if(result.hasErrors()) {
+            model.addAttribute("newUser", new User());
+            return "index.jsp";
+        }
+    
+        // No errors! 
+        // Store their ID from the DB in session, 
+        // in other words, log them in.
+        session.setAttribute("userId", user.getId());
+    
+        return "redirect:/home";
+    }
+    
+    @GetMapping("/home")
+    public String home(Model model, HttpSession session) {
+    	
+    	if(session.getAttribute("userId") == null) {
+    		return "redirect:/";
+    	}
+    	
+    	model.addAttribute("books", books.all());
+    	model.addAttribute("user", users.findById((Long)session.getAttribute("userId")));
+    	return "home.jsp";
+    }
+    
+    @GetMapping("/addPage")
+    public String addPage(@ModelAttribute("book") Book book, Model model, HttpSession session) {
+    	
+    	User user = users.findById((Long)session.getAttribute("userId"));
+    	model.addAttribute("user", user);
+    	
+    	return "new.jsp";
+    }
+    
+    @PostMapping("/books")
+    public String createBook(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+
+    	if (result.hasErrors()) {
+    		return "new.jsp";
+    	}
+    	
+    	books.create(book);
+    	
+    	return "redirect:/home";
+    }
+    
+    @GetMapping("/books/{id}/edit")
+    public String editPage(Model model, @PathVariable("id") Long id, HttpSession session) {
+    	
+    	if(session.getAttribute("userId") == null) {
+    		return "redirect:/";
+    	}
+    	
+    	Book book = books.findById(id);
+    	model.addAttribute("book", book);
+    	
+    	return "edit.jsp";
+    }
+    @GetMapping("/books/{id}")
+    public String showPage(Model model, @PathVariable("id") Long id, HttpSession session) {
+    	if(session.getAttribute("userId") == null) {
+    		return "redirect:/";
+    	}
+    	Book book = books.findById(id);
+    	model.addAttribute("book", book);
+    	model.addAttribute("user", users.findById((Long)session.getAttribute("userId")));
+    	
+    	return "show.jsp";
+    }
+    
+    @PutMapping("/books/{id}")
+    public String updateBook(@Valid @ModelAttribute("book") Book book, BindingResult result, Model model) {
     	
     	if (result.hasErrors()) {
     		return "edit.jsp";
     	}
     	
-    	System.out.println("Book being saved to DB: " + book.getId());
+    	System.out.println("Book to be saved to DB: " + book.getId());
 
         books.update(book);
         
     	
     	return "redirect:/home";
     }
-	
-	
-	@GetMapping("/books/{id}/edit")
-	public String editPage(Model model, @PathVariable("id") Long id, HttpSession session) {
-		if (session.getAttribute("userId") == null) {
-			return "redirect:/";
-		}
-		Book book = books.findById(id);
-		model.addAttribute("book", book);
-		return "edit.jsp";
-	}
-	
-	@GetMapping("/logout")
-	public String logout(HttpSession session) {
-		session.invalidate();
-		return "redirect:/";
-	}
+    
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+    	session.invalidate();
+    	return "redirect:/";
+    }
+    
 }
