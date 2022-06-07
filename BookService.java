d89f524acebb56ce07ec4545aeaf8a43db8c36f7
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-06-07 14:18:28 -0700

    Update BookService.java

--- BookService.java
+++ BookService.java
@@ -1,12 +1,12 @@
 package com.codingdojo.bookclub.services;
 
-import java.awt.print.Book;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.codingdojo.bookclub.models.Book;
 import com.codingdojo.bookclub.repositories.BookRepository;
 
 @Service
